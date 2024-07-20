6bd9f46dc0299c52edbe8ed12cf82ed9673b3865
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-07-20 18:27:59 +0300

    fix: state update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test, setTest] = useState({ name: "Ziha" });
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
+    // setTest({ name: "Kaneza" });
+  }
+
+  // function handleClose() {
+  //   if (isOpen) setIsOpen(false);
+  // }
+
   return (
-    <div className="steps">
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <main className="steps">
+          <section className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </section>
+
+          <p className="message">
+            Steps {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+
+          <section className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </section>
+        </main>
+      )}
+    </>
   );
 }
